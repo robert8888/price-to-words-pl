76d757d51cf46e8c06f96ccd78caebf9cc995572
PWR\r.kaminski.pwr <r.kaminski@pwr.agro.pl>
2021-10-13 14:45:41 +0200

    Fix library export

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,13 @@
-// Webpack uses this to work with directories
-import path from "path"
-import { fileURLToPath } from 'url';
+const path = require('path')
 
-// This is the main configuration object.
-// Here, you write different options and tell Webpack what to do
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-export default {
+module.exports = {
     entry: './src/index.js',
 
     output: {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '',
         filename: 'index.js',
-        library: 'price-to-words-pl',
+        library: 'PriceToWordsPl',
         libraryTarget: 'umd',
         libraryExport: 'default',
         globalObject: 'this',
